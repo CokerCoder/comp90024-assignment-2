204458eb4e0c1d74bf9db643b15dbd66851cf1ea
Haoyuan <haoyuan.yu@outlook.com>
2021-04-26 13:45:49 +0800

    clean file; change host.ini

--- ansible/deploy_servers.sh
+++ ansible/deploy_servers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./grp-26-openrc.sh; ansible-playbook -e "configure_cluster=True" deploy_servers.yaml -i inventory/hosts.ini
+./grp-26-openrc.sh; ansible-playbook deploy_servers.yaml -i inventory/hosts.ini
