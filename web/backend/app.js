6896ac4deb815c76f0350a828d0abc9ab962f60d
Haoyuan Yu <haoyuan.yu@outlook.com>
2021-05-08 13:35:37 +0800

    refactor: :bug:

--- web/backend/app.js
+++ web/backend/app.js
@@ -1,57 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var cors = require('cors')
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cors = require("cors");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var get_all = require("./routes/get_all");
 var get_all_cities = require("./routes/get_all_cities");
 var get_all_suburbs = require("./routes/get_all_suburbs");
-var testAPI = require("./routes/test.js");
-var couchDB = require("./routes/couch.js");
+var get_coordinates = require("./routes/get_coordinates");
+var couchDB = require("./routes/couchdb.js");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.disable('etag');
+app.disable("etag");
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/get_all',get_all);
-app.use('/get_all_cities',get_all_cities);
-app.use('/get_all_suburbs',get_all_suburbs);
-app.use('/test', testAPI);
-app.use('/couch', couchDB);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/get_all", get_all);
+app.use("/get_coordinates", get_coordinates);
+app.use("/get_all_cities", get_all_cities);
+app.use("/get_all_suburbs", get_all_suburbs);
+app.use("/couchdb", couchDB);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
-app.listen(8000, function(){
-    console.log("listening")
-})
+app.listen(8000, function () {
+  console.log("listening");
+});
--- web/backend/routes/get_all.js
+++ web/backend/routes/get_all.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-//const env_ip = process.env.COUCHDB_HOST
-
-//ip = env_ip.replace(/"/g,"");
-
-ip = "http://admin:data-miner!@172.26.133.36:5984"
-
-
+ip = "http://admin:admin@172.26.132.83:5984";
 
 const nano = require("nano")(ip);
 
-router.get("/",async function(req, res, next){
-    
-    var shape = [] 
-    let id = req.query.id;
-    var db = nano.use('postcode_aurin')
-    await db.view("get_all","id").then((body) => {
-        body.rows.forEach((doc) => {
-            console.log(doc.value);
-            shape.push(doc.value);
-        });
+router.get("/", async function (req, res, next) {
+  var shape = [];
+  let id = req.query.id;
+  var db = nano.use("postcode_aurin");
+  await db.view("get_all", "id").then((body) => {
+    body.rows.forEach((doc) => {
+      console.log(doc.value);
+      shape.push(doc.value);
     });
-    res.send(shape);
+  });
+  res.send(shape);
 });
 
 module.exports = router;
