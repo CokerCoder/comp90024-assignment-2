17f25349a6c471f56a20d53a3514cdba300d9761
Echoid <youranzhou0724@gmail.com>
2021-05-10 00:48:24 +1000

    AURIN education and sports

--- crawler/aurin.py
+++ crawler/aurin.py
@@ -1,6 +1,9 @@
 import json
 import couchdb
 from by_realtime.crawl_util import find_or_create_db
+from collections import defaultdict
+from find_location import find_location
+from tqdm import tqdm
 
 SERVER = couchdb.Server('http://admin:admin@172.26.132.83:5984/')
 
@@ -18,13 +21,49 @@ def load_profile():
         except:
             pass
 
-def load_education():
-    database = find_or_create_db(SERVER, "education")
-    education = json.load(open('education.json'))
+def load_education_TAFE():
+    database = find_or_create_db(SERVER, "education_tafe")
+    education = json.load(open('data/education-TAFE.json'))
+    TAFEdic = defaultdict(lambda:0)
+    for i in education["features"]:
+        if i["properties"]["state"] == "VIC":
+            coor = [i["properties"]["longitude"],i["properties"]["latitude"]]
+            lga = find_location(coor)
+            if lga:
+                TAFEdic[lga] += 1
+
+    for i in TAFEdic:
+        database.save({"_id":i,
+                    "TAFE":TAFEdic[i] })
+
+
+def load_education_UNI():
+    database = find_or_create_db(SERVER, "education_uni")
+    education = json.load(open('data/education-UNI.json'))
+    UNIdic = defaultdict(lambda:0)
+    for i in education["features"]:
+        if i["properties"]["state"] == "VIC":
+            coor = [i["properties"]["longitude"],i["properties"]["latitude"]]
+            lga = find_location(coor)
+            if lga:
+                UNIdic[lga] += 1
+
+    for i in UNIdic:
+        database.save({"_id":i,
+                    "UNI":UNIdic[i] })
 
 def load_sport():
     database = find_or_create_db(SERVER, "sport")
-    sport = json.load(open('education.json'))
+    sport = json.load(open('data/sport.json'))
+    sportdic = defaultdict(lambda:0)
+    for i in tqdm(sport["features"]):
+        location = find_location(i["geometry"]["coordinates"])
+        if location:
+            sportdic[location] += 1
+
+    for i in sportdic:
+        database.save({"_id":i,
+                 "SPORT":sportdic[i] })
 
 def load_network():
     database = find_or_create_db(SERVER, "network")
--- crawler/find_location.py
+++ crawler/find_location.py
@@ -5,7 +5,7 @@ from shapely.geometry.polygon import Polygon
 
 
 
-Greater_Melbourne_POLYGON = json.load(open('Greater_Melbourne_LGA.json'))["features"]
+Greater_Melbourne_POLYGON = json.load(open('data/Greater_Melbourne_LGA.json'))["features"]
 
 
 def find_location(coor):
@@ -16,29 +16,3 @@ def find_location(coor):
             if Polygon(subpolygon[0]).contains(Point(coor)):
                 return suburb['properties']["vic_lga__3"]
     return None
-
-
-#####################################################################
-
-DATABASE = 'tweet_docs'
-SERVER = couchdb.Server('http://admin:admin@172.26.132.83:5984/')
-
-view = ViewCreator(DATABASE, SERVER)
-
-locationlist = []
-
-
-for doc in view.get_tweets():
-    
-    coor = [(doc.value["Coordinates"][0][0]+doc.value["Coordinates"][2][0])/2,(doc.value["Coordinates"][0][1]+doc.value["Coordinates"][1][1])/2]
-
-    find_location(coor)
-    ## Return 一个LGA, 已经清理了too genral的location
-
-    # database.save({'_id': doc.key,
-    #                     'name': doc.value['Name'],
-    #                     'coordinates': doc.value['Coordinates'],
-    #                     'sentiment': sia.polarity_scores(doc.value['Text']),
-    #                     'location' :find_location(coor)
-    #                 })
-    
