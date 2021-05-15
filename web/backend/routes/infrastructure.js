39dbd8967972da008b22300ed4f3deb0070191c0
Haoyuan Yu <haoyuan.yu@outlook.com>
2021-05-15 19:02:45 +0800

    feat: :sparkles: add more features in infrastructure

--- web/backend/routes/infrastructure.js
+++ web/backend/routes/infrastructure.js
@@ -9,6 +9,8 @@ router.get("/", async function (req, res, next) {
   var sport = [];
   var uni = [];
   var tafe = [];
+  var school = [];
+  var hospital = [];
 
   var db_sport = nano.use("sport");
   await db_sport.list({ include_docs: true }).then((body) => {
@@ -35,7 +37,6 @@ router.get("/", async function (req, res, next) {
       uni.push(dict);
     });
   });
-  console.log(uni);
   var db_tafe = nano.use("education_tafe");
   await db_tafe.list({ include_docs: true }).then((body) => {
     body.rows.forEach((doc) => {
@@ -49,6 +50,32 @@ router.get("/", async function (req, res, next) {
     });
   });
 
+  var db_school = nano.use("education_school");
+  await db_school.list({ include_docs: true }).then((body) => {
+    body.rows.forEach((doc) => {
+      doc = doc.doc;
+      let dict = {
+        id: doc._id,
+        school: doc.School,
+      };
+
+      school.push(dict);
+    });
+  });
+
+  var db_hospital = nano.use("national_hospital");
+  await db_hospital.list({ include_docs: true }).then((body) => {
+    body.rows.forEach((doc) => {
+      doc = doc.doc;
+      let dict = {
+        id: doc._id,
+        hospital: doc["National Hospital"],
+      };
+
+      hospital.push(dict);
+    });
+  });
+
   var uni_ref = uni.reduce(function (obj, o) {
     obj[o.id] = o.uni;
     return obj;
@@ -59,6 +86,16 @@ router.get("/", async function (req, res, next) {
     return obj;
   }, {});
 
+  var school_ref = school.reduce(function (obj, o) {
+    obj[o.id] = o.school;
+    return obj;
+  }, {});
+
+  var hospital_ref = hospital.reduce(function (obj, o) {
+    obj[o.id] = o.hospital;
+    return obj;
+  }, {});
+
   var infrastructure = sport.map(function (o) {
     if (uni_ref[o.id] === undefined) {
       uni_ref[o.id] = 0;
@@ -66,11 +103,16 @@ router.get("/", async function (req, res, next) {
     if (tafe_ref[o.id] === undefined) {
       tafe_ref[o.id] = 0;
     }
+    if (hospital_ref[o.id] === undefined) {
+      hospital_ref[o.id] = 0;
+    }
     return {
       id: o.id,
       sport: o.sport,
       uni: uni_ref[o.id],
       taft: tafe_ref[o.id],
+      school: school_ref[o.id],
+      hospital: hospital_ref[o.id],
     };
   });
 
