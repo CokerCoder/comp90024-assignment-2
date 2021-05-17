ca088bceaf79f7aec42e6cd95a598e707c3d7b2d
Yunfei Jing <yunfei.jing@outlook.com>
2021-05-17 10:28:41 +1000

    use env files to seperate local and dev env

--- web/frontend/src/commons/axios.js
+++ web/frontend/src/commons/axios.js
@@ -1,8 +1,10 @@
 import _axios from 'axios';
 
+const devEnvironment = process.env.REACT_APP_ENDPOINT;
+
 const axios = baseUrl => {
     const instance = _axios.create({
-        baseURL: baseUrl || 'http://172.26.132.181:8000',
+        baseURL: devEnvironment,
         timeout: 20000,
     });
     return instance;
