ebad164cd5ca8712abca23483cb769382325ba8b
Haoyuan Yu <haoyuan.yu@outlook.com>
2021-05-17 21:40:01 +0800

    style: :lipstick: pretty front end

--- web/frontend/src/pages/App.js
+++ web/frontend/src/pages/App.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom'
-import { Button } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/App.css';
-import uni_image from '../image/uni.png';
-import mel_image from '../image/mel_black.png';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/App.css";
+import uni_image from "../image/uni.svg";
+import mel_image from "../image/mel_black.png";
 
+function App() {
+  let history = useHistory();
 
-
-function App() { 
-  let history = useHistory()
-
-  const toMap = () => (
+  const toMap = () =>
     history.push({
-      pathname:"/map"
-    })
-  );
+      pathname: "/map",
+    });
 
   return (
     <>
       <div className="project-name">
-        <img src={uni_image} className="uni_image" />
-        <h1>COMP90024 CCC Project-2</h1>
+        <a href="https://www.unimelb.edu.au/">
+          <img src={uni_image} className="uni_image" />
+        </a>
+        <h1 />
+        <h2>COMP90024</h2>
       </div>
       <img src={mel_image} className="mel_image" />
       <div className="start-area">
-        <h2>Infrastructure VS Sentiment scores</h2>
-        <h3>Presented by Group-26 </h3>
+        <h2>Infrastructure VS Sentiment Scores</h2>
+        <h3>Group-26</h3>
         <br></br>
         <Button variant="success" onClick={toMap}>
-          Start explore
+          Start Explore
         </Button>
       </div>
     </>
