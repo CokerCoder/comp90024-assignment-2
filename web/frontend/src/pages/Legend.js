1e4ceb01d68fc32c6bc57385f69c9c05ca575816
Matt <tianze@student.unimelb.edu.au>
2021-05-24 15:25:12 +0800

    ✨ Add comments

--- web/frontend/src/pages/Legend.js
+++ web/frontend/src/pages/Legend.js
@@ -1,3 +1,12 @@
+// COMP90024-Cluster and Cloud Computing Assignment 2
+// Team Group 26
+
+// Yunfei Jing (987784) jinyj@student.unimelb.edu.au
+// Tianze Liu (987969) tianze@student.unimelb.edu.au
+// Liang Min(981061) lmmin@student.unimelb.edu.au
+// Youran Zhou(991504) youran@student.unimelb.edu.au
+// Haoyuan Yu (988290) haoyuany@student.unimelb.edu.au
+
 import { useMap } from "react-leaflet";
 import L from "leaflet";
 import { useEffect } from "react";
